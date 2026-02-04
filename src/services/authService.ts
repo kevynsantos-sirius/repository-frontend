e0e58f58c570e2aafa1e851e1540781efa439083
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-02-04 18:44:06 -0300

    FEAT: Ajustando requisição do login
     https://sirius-team-ch6cxuf1.atlassian.net/browse/PADD-7

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,14 +1,14 @@
 import { api } from './api'
 
 export async function login(usuario: string, senha: string) {
-  const response = await api.post('/login', {
-    usuario,
-    senha
+  const response = await api.post('/api/auth/login', {
+    username: usuario,
+    password: senha
   })
 
   return response.data
 }
 
 export async function logout() {
-  await api.post('/logout')
+  await api.post('/api/auth/logout')
 }
